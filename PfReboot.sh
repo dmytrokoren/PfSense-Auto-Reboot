2787e9e9a2509b8ea39400ae910065a5149e1555
Dmytro Korenkov <dmytrokoren@gmail.com>
2024-03-04 01:28:28 -0600

    Latest final commit

---
+++ PfReboot.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+#==================================================================
+# description     :Script monitors primary and secondary DNS pings.
+#                  If an issue arises, it temporarily resets the
+#                  WAN interface and, if needed, triggers a system
+#                  reboot, accompanied by a Telegram notification.
+#==================================================================
+
+# Function to be executed on error or termination
+cleanup() {
+    counter_file="/usr/local/bin/PfReboot_count.txt"
+    echo "ready" >>"$counter_file"
+    if [ $? -eq 130 ]; then
+        echo -e "\nScript encountered an error"
+    else
+        echo -e "\nScript terminated by user"
+    fi
+    exit 1
+}
+
+# Set up a trap to catch ERR and SIGINT signals
+trap cleanup ERR INT
+
+counter_file="/usr/local/bin/PfReboot_count.txt"
+
+# Check if the file exists
+if [ ! -e "$counter_file" ]; then
+    echo "0" >"$counter_file"
+    echo "ready" >>"$counter_file"
+fi
+
+# Read the second line of the file
+current_status=$(sed -n '2p' "$counter_file")
+
+# Check if the second line contains "ready"
+if [[ -z "$current_status" || "$current_status" != "ready" ]]; then
+    #echo "The current status inside of counter_file does not contain 'ready'. Exiting the script."
+
+    # Uncomment the following lines if you want feedback on the console
+    #echo "The current status is not 'ready'. Exiting the script." >>pfreboot_status.txt
+    #wall pfreboot_status.txt
+    #rm pfreboot_status.txt
+    exit 0
+fi
+
+# Remove the second line from the file
+awk 'NR!=2' "$counter_file" >temp_file && mv temp_file "$counter_file"
+
+# Max reboots before sleep
+max_reboots=2
+
+# Read the first line of the file
+current_count=$(head -n 1 "$counter_file" 2>/dev/null)
+
+# Increment and save counter function
+increment_counter() {
+    current_count=$(head -n 1 "$counter_file" 2>/dev/null)
+    [ -z "$current_count" ] && current_count=0
+    ((current_count++))
+    echo "$current_count" >"$counter_file"
+}
+
+update_status_state() {
+    if ! grep -q "ready" "$counter_file"; then
+        echo "ready" >>"$counter_file"
+    fi
+}
+
+# Testing uptime to run script only xx seconds after boot
+
+# Current time
+currtime=$(date +%s)
+
+# Boot time in seconds
+utime=$(sysctl kern.boottime | awk -F'sec = ' '{print $2}' | awk -F',' '{print $1}')
+
+# Uptime in seconds
+utime=$(($currtime - $utime))
+
+# If boot is longer than 120 seconds ago... (To avoid bootloops)
+if [ $utime -gt 120 ]; then
+    # Uncomment the following lines if you want feedback on the console
+    #echo "Testing Connection at" $(date +%Y-%m-%d.%H:%M:%S) "uptime:" $utime "seconds" >>pfreboot_log.txt
+    #wall pfreboot_log.txt
+    #rm pfreboot_log.txt
+
+    # Try 1 or 2 minutes worth of very short pings to Cloudflare public DNS servers
+    # Quit immediately if we get a single frame back
+    # If neither server responds at all, then reboot the firewall
+
+    firstDNS="1.0.0.1"
+    secondDNS="1.1.1.1"
+
+    counting=$(ping -o -s 0 -c 10 $firstDNS | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+
+    if [ $counting -eq 0 ]; then
+        php -r 'require_once("/etc/inc/notices.inc"); notify_via_telegram("Ping to DNS server '$firstDNS' is unreachable.");'
+
+        counting=$(ping -o -s 0 -c 10 $secondDNS | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+
+        if [ $counting -eq 0 ]; then
+            php -r 'require_once("/etc/inc/notices.inc"); notify_via_telegram("Ping to DNS server '$secondDNS' is unreachable.");'
+
+            # Trying to restart NIC
+            # Change the NIC name as per your WAN
+            nic_name="re0"
+
+            php -r 'require_once("/etc/inc/notices.inc"); notify_via_telegram("Restarting WAN ('$nic_name')");'
+
+            ifconfig $nic_name down
+            ifconfig $nic_name up
+
+            # Check if re0 is up
+            while ! ifconfig $nic_name | grep -q "status: active"; do
+                sleep 1
+            done
+
+            sleep 10s
+
+            # Testing if a ping is successful
+            counting=$(ping -o -s 0 -c 10 $firstDNS | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+
+            if [ $counting -eq 0 ]; then
+                php -r 'require_once("/etc/inc/notices.inc"); notify_via_telegram("Ping to DNS server '$firstDNS' remains unreachable even after WAN ('$nic_name') restart.");'
+
+                if [ "$current_count" -ge "$max_reboots" ]; then
+                    php -r 'require_once("/etc/inc/notices.inc"); notify_via_telegram("The maximum number of reboots ('$max_reboots') has been reached. The system will wait for 1 hour before the next reboot interval.");'
+
+                    sleep 1h
+
+                    # Reset the counter to 0
+                    echo "0" >"$counter_file"
+                fi
+
+                # Increment counter and write to file
+                increment_counter
+
+                update_status_state
+
+                # Save RRD data
+                # Reboot the system
+                php -r 'require_once("/etc/inc/notices.inc"); notify_via_telegram("pfSense is rebooting now.");'
+                /etc/rc.backup_rrd.sh
+                reboot
+            fi
+        fi
+    fi
+
+    update_status_state
+
+fi
---
+++ README.md
@@ -0,0 +1,89 @@
+# PfSense-Auto-Reboot (Tested on pfSense 2.7.2)
+
+A simple script to reboot your pfSense box when it is not connected to the internet.
+
+This guide is extremely beginner friendly. I wish I had it, when I was starting out.
+
+- Copy the script to the local pfsense router.
+
+  1. Login via ssh using root user and password.
+
+  2. Select shell command `8`.
+
+  3. Change your directory to /usr/local/bin.
+
+  ```
+  cd /usr/local/bin
+  ```
+
+  4. Download the script file.
+
+  ```
+  curl -LJO https://raw.githubusercontent.com/dmytrokoren/PfSense-Auto-Reboot/main/PfReboot.sh
+  ```
+
+  5. Install nano file editor
+
+  ```
+  pkg update
+  pkg install nano
+  ```
+
+  6. Change your wan adapter name, if required (mine is re0) using nano
+
+  ```
+  nano PfReboot.sh
+  ```
+
+  7. (OPTONAL) Change the script to customize your experience.
+
+     - Change the public ip of cloudflare to your liking (public server). But make sure it is a always on public ip address and does respond to ping (ICMP).
+     - Uncomment the print lines. if you want to see feedback on the console. But by-default it is off.
+
+  8. After Pasting the Script. Press `ESC` then `:x` to exit from the vi editor.
+
+- To test the script on your local pfsense box.
+
+  1. Install bash, if not installed already.
+
+  ```
+  pkg install bash
+  ```
+
+  2. Change permisson to executable.
+
+  ```
+  chmod +x PfReboot.sh
+  ```
+
+  3. Run it as "bash PfReboot.sh".
+
+  ```
+  bash PfReboot.sh
+  ```
+
+- To run the the script on your local pfsence box on schedule.
+
+  1. Install cron, if not installed already.
+
+  System > Package Manager > Available Packages > Search "cron" > install.
+
+  2. Configure the cron service.
+
+  Services > corn > add
+
+  Then type as follows
+
+      - Minute - *
+      - Hour - *
+      - Day of the month - *
+      - Month of the year - *
+      - Day of the week - *
+      - User -  root
+      - Command - ``` bash /usr/local/bin/PfReboot.sh ```
+
+  3. Click on Save.
+
+Now we are done. You can sleep peacefully and dont have to press the reset button when the internet is goes down. It will automatically reboot itself within sometime of going offline.
+
+Feedback is always welcome.
